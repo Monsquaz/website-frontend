d43ed84a1f9a7918d34dd57aa22ad9a298d8f158
Patrik Weibull <patrik.weibull@gmail.com>
2017-07-04 13:06:57 +0000

    Worked ofurther on structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
 const path = require('path');
+const webpack = require('webpack');
+
+var isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist/',
+    path: path.resolve(__dirname, 'public/js'),
+    publicPath: 'js/',
     filename: 'build.js'
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, "public"),
     compress: true,
     port: 9000,
     host: "0.0.0.0",
@@ -16,6 +19,11 @@ module.exports = {
       'monsquaz.org'
     ]
   },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
+    })
+  ],
   module: {
     loaders: [
       {
